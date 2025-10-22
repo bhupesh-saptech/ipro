c0a14382fc34a667a786a11a49ba4036854def7e
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-10-22 23:26:59 +0530

    .

--- application/models/Assets.php
+++ application/models/Assets.php
@@ -1,9 +1,10 @@
 <?php
 class Assets extends CI_Model {
     public function read_data() {
-        $this->db->select('a.*,b.site_no,b.site_name');
+        $this->db->select('a.*,b.site_no,b.site_name,c.staff_name');
         $this->db->from( 'assets a');
         $this->db->join( 'sites  b', 'b.site_id = a.site_id', 'left');
+        $this->db->join( 'staff  c', 'c.staff_id = a.staff_id', 'left');
         return $this->db->get()->result();
     }
 }
