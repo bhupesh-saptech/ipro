1990c3e7f7f67fbce5568005836123dee75b8ddc
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-10-22 12:33:42 +0530

    added html code

--- application/views/Home.php
+++ application/views/Home.php
@@ -1,4 +1,12 @@
 <?php 
-    echo "<h1 align='center'>Welcome to AgiLabPlus Inventech</h1>";
+    
 ?>
+<html>
+    <head>
+        <title>Welcome to AgilabPlus Inventech<title>
+    </head>
+    <body>
+        <h1 align='center'>Welcome to AgiLabPlus Inventech</h1>
+    </body>
+</html>
 
