be960ab924c11b8068932c70478eb8e8ee1516ce
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-10-22 23:43:31 +0530

    .

--- application/views/Home.php
+++ application/views/Home.php
@@ -5,7 +5,7 @@
     <h1 class="text-center">Welcome to AgileLabPlus</h1>
     <div class="container">
         <table class="table table-bordered">
-            <tr class="table-primary">
+            <tr class="table-dark text-white">
                 <th>#</th>
                 <th>asset ID</th>
                 <th>asset No</th>
