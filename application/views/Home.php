1b6e94eaf0709d6cf7bb877cc6b052595e036a89
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-10-22 13:05:50 +0530

    added html code

--- application/views/Home.php
+++ application/views/Home.php
@@ -1,5 +1,15 @@
 <?php 
- echo "<h1 align='center'>Welcome to AgiLabPlus Inventech</h1>";    
-?>
+    defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+</head>
+<body>
+    <h1 align='center'>Welcome to AgiLabPlus Inventech</h1>
+</body>
+</html>    
+
 
 
