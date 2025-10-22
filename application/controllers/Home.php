2f9512f8530fef3d7860584ec63ddccb0fd50690
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-10-23 04:48:45 +0530

    .

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends CI_Controller {
         //  echo "<pre>";
         //  print_r($data['assets']);
 
-       $this->load->view("Home",$data);
+       $this->load->view("assets",$data);
     }
 
 }
