aafde86f32b7103e969aa534ea4535cfa3f83381
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-10-22 14:34:11 +0530

    added files

--- application/views/Home.php
+++ application/views/Home.php
@@ -1,6 +1,8 @@
 <?php 
     defined('BASEPATH') OR exit('No direct script access allowed'); 
     require 'incld/header.php';
+    require 'incld/top_menu.php';
+    require 'incld/jslib.php';
     require 'incld/footer.php';
 ?>
 
