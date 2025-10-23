41defb35978e0c8bf9a97a677bf70b4d0ad1d555
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-10-23 13:13:10 +0530

    get data function modified

--- application/models/Assets.php
+++ application/models/Assets.php
@@ -1,11 +1,16 @@
 <?php
 class Assets extends CI_Model {
-    public function read_data() {
+   
+    public function get_data($id = '') {
+        
         $this->db->select('a.*,b.site_no,b.site_name,c.staff_name');
         $this->db->from( 'assets a');
         $this->db->join( 'sites  b', 'b.site_id = a.site_id', 'left');
         $this->db->join( 'staff  c', 'c.staff_id = a.staff_id', 'left');
-        return $this->db->get()->result();
+        if($id != '') {
+            $this->db->where('a.id', '$id');
+        }
+        return $this->db->get()->row();
     }
 }
 ?>
