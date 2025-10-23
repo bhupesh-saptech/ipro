52fb2628c9fc3d993d68021dd324123d3638fa9e
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-10-23 20:12:53 +0530

    AssetModel Changed

--- application/models/AssetModel.php
+++ application/models/AssetModel.php
@@ -7,10 +7,12 @@ class AssetModel extends CI_Model {
         $this->db->from( 'assets a');
         $this->db->join( 'sites  b', 'b.site_id = a.site_id', 'left');
         $this->db->join( 'staff  c', 'c.staff_id = a.staff_id', 'left');
-        // if($id != '') {
-        //     $this->db->where('a.id', '$id');
-        // }
-        return $this->db->get()->result();
+        if($id != '') {
+             $this->db->where('a.id', '$id');
+             return $this->db->get()->row();
+        } else {
+            return $this->db->get()->result();
+        }
     }
 }
 ?>
