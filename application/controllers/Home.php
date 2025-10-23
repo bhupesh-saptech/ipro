16f8edbd0499125a25c72efbe30cea3b2ac20a33
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-10-23 13:16:04 +0530

    Home controller Changed

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,9 +1,7 @@
 <?php
 class Home extends CI_Controller {
     public function index() {
-        $this->load->model('assets');
-        $data['assets'] = $this->assets->read_data();
-       $this->load->view("assets/list",$data);
+
     }
 
 }
