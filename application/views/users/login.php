21a20a9e6a88c98bbe39777a8603b6edcf0f9ee5
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-10-25 18:12:00 +0530

    login page modified

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>CodeInsect | Admin System Log in</title>
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    <link href="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="<?php echo base_url(); ?>assets/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <link href="<?php echo base_url(); ?>assets/dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+<?php 
+    defined('BASEPATH') OR exit('No direct script access allowed'); 
+    require 'assets/incld/header.php';
+?>
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <!-- Google Font -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-  </head>
   <body class="hold-transition login-page">
     <div class="login-box">
       <div class="login-logo">
@@ -78,7 +64,7 @@
       </div><!-- /.login-box-body -->
     </div><!-- /.login-box -->
 
-    <script src="<?php echo base_url(); ?>assets/bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-  </body>
-</html>
+<?php
+    require 'assets/incld/jslib.php';
+    require 'assets/incld/footer.php';
+?>
