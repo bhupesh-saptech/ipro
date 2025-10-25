65e19f885cb5cdd5b47633178cd39f8fafb86b8e
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-10-25 22:20:37 +0530

    asset cont changed

--- application/controllers/Asset.php
+++ application/controllers/Asset.php
@@ -2,22 +2,22 @@
 class Asset extends CI_Controller {
     public function index() {
         $this->load->model('Asset_model');
-        $data['assets'] = $this->asset_model->get_data();
+        $data['assets'] = $this->Asset_model->get_data();
         $this->load->view("asset/list",$data);
     }
     public function view($asset_id="") {
         $this->load->model('Asset_model');
-        $data['asset'] = $this->asset_model->get_data($asset_id);
+        $data['asset'] = $this->Asset_model->get_data($asset_id);
         $this->load->view("asset/view",$data);
     }
     public function edit($asset_id="") {
         $this->load->model('Asset_model');
-        $data['asset'] = $this->asset_model->get_data($asset_id);
+        $data['asset'] = $this->Asset_model->get_data($asset_id);
          $this->load->view("asset/view",$data);
     }
     public function delete($asset_id="") {
         $this->load->model('Asset_model');
-        $data['asset'] = $this->asset_model->get_data($asset_id);
+        $data['asset'] = $this->Asset_model->get_data($asset_id);
          $this->load->view("asset/view",$data);
     }
 
