d421d274f3cc49d98863d3da23150748ca3c6936
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-10-25 22:18:06 +0530

    asset controller modified

--- application/controllers/Asset.php
+++ application/controllers/Asset.php
@@ -1,23 +1,23 @@
 <?php
 class Asset extends CI_Controller {
     public function index() {
-        $this->load->model('AssetModel');
-        $data['assets'] = $this->AssetModel->get_data();
+        $this->load->model('Asset_model');
+        $data['assets'] = $this->asset_model->get_data();
         $this->load->view("asset/list",$data);
     }
     public function view($asset_id="") {
-        $this->load->model('AssetModel');
-        $data['asset'] = $this->AssetModel->get_data($asset_id);
+        $this->load->model('Asset_model');
+        $data['asset'] = $this->asset_model->get_data($asset_id);
         $this->load->view("asset/view",$data);
     }
     public function edit($asset_id="") {
-        $this->load->model('AssetModel');
-        $data['asset'] = $this->AssetModel->get_data($asset_id);
+        $this->load->model('Asset_model');
+        $data['asset'] = $this->asset_model->get_data($asset_id);
          $this->load->view("asset/view",$data);
     }
     public function delete($asset_id="") {
-        $this->load->model('AssetModel');
-        $data['asset'] = $this->AssetModel->get_data($asset_id);
+        $this->load->model('Asset_model');
+        $data['asset'] = $this->asset_model->get_data($asset_id);
          $this->load->view("asset/view",$data);
     }
 
