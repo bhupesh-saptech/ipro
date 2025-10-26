28ca2844ccd44e9eef5c610fbded5adafd843d19
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-10-26 21:40:23 +0530

    Color changed

--- application/views/general/dashboard.php
+++ application/views/general/dashboard.php
@@ -15,7 +15,7 @@
         <div class="row">
             <div class="col-lg-3 col-xs-6">
               <!-- small box -->
-              <div class="small-box bg-aqua">
+              <div class="small-box bg-primary">
                 <div class="inner">
                   <h3>150</h3>
                   <p>New Tasks</p>
