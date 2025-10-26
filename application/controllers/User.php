5b5d403778f7d5f3b221f3afdd44d8739684bae9
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-10-26 21:51:08 +0530

    Global Added

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -27,7 +27,7 @@ class User extends BaseController
     public function index()
     {
         $this->global['pageTitle'] = 'CodeInsect : Dashboard';
-        $this->load->view("general/dashboard");
+        $this->load->view("general/dashboard",$this->global);
         
         // $this->loadViews("general/dashboard", $this->global, NULL , NULL);
     }
